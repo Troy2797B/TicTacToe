62ace928c583aa755c152b767be3e5fb2655538a
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-11-08 23:23:04 -0500

    columns done

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,63 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    public Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        if (getWinner() == "X"){
+            return true;
+        } return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if (getWinner() == "O"){
+            return true;
+        } return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if (!isInFavorOfO() && !isInFavorOfX()) {
+            return true;
+        } return false;
     }
 
     public String getWinner() {
-        return null;
-    }
+        String winner = "";
+        for (int i = 0; i < matrix.length; i++){
+            for (int k = 0; k < matrix.length;i++){
+                if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X'){
+                    winner ="X";
+                    break;
+                }
+                else if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O') {
+                    winner = "O";
+                    break;
+                }
+                //first column wins for both done
+                else if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X') {
+                    winner = "X";
+                    break;
+                }
+                else if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O') {
+                    winner = "O";
+                    break;
+                }
+                //second column done
+                else if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X') {
+                    winner = "X";
+                    break;
+                }
 
+                else if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O') {
+                    winner = "O";
+                    break;
+                }//COLUMNS COMPLETELEY DONE
+            }
+        }
+        return winner;
+    }
 }
